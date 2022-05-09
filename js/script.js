58013ab46627c91ecd14fb40ece24ac355628d88
keyukidau4 <keyukidau4@gmail.com>
2022-05-10 02:39:17 +0900

    5.9 (trending section)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 let menuIcon = document.querySelector(".menu-icon");
 let menu = document.querySelector(".menu");
 
-
 menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle("move");
-    menu.classList.toggle("active");
+  menuIcon.classList.toggle("move");
+  menu.classList.toggle("active");
 });
 
 // Notification
@@ -12,13 +11,35 @@ menuIcon.addEventListener("click", () => {
 let bell = document.querySelector(".notification");
 let bellIcon = document.querySelector("#bell-icon");
 
-bellIcon.addEventListener("mouseover",()=>{
-    bell.classList.add("active");
+bellIcon.addEventListener("mouseover", () => {
+  bell.classList.add("active");
 });
 
-bellIcon.addEventListener("mouseout",()=>{
-    bell.classList.remove("active");
+bellIcon.addEventListener("mouseout", () => {
+  bell.classList.remove("active");
 });
 
+// slide swiper
 
-
+var swiper = new Swiper(".trending-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1068: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
